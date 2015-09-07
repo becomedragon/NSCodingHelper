0a29fb2c04dcec9faba4db45561b91dd2b4eb82c
lawrence <becomedragonlong@gmail.com>
2015-09-07 10:29:03 +0800

    Initial Commit

---
+++ property/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  property
+//
+//  Created by lawrence on 9/7/15.
+//  Copyright (c) 2015 lawrence. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
